a53c668587c9f651799b1c16195c7588585796cf
Adel Akbar <adel-s@outlook.com>
2024-10-08 20:49:42 -0700

    Slack Clone

---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { toast } from "sonner";
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export const CreateWorkspaceModal = () => {
+  const router = useRouter();
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
+
+  const { mutate, isPending } = useCreateWorkspace();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name },
+      {
+        onSuccess(id) {
+          toast.success("Workspace created");
+          router.push(`/workspace/${id}`);
+          handleClose();
+        },
+      }
+    );
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setName("");
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a workspace</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
